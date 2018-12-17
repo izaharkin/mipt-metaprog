3b724ad14d8cc645d94e28be65dcc8712cbe08d5
izaharkin <ilyazaharkin@yandex.ru>
2018-12-17 20:11:30 +0300

    ADD: test file and test screenshot gor hw3

--- [hw3_lecture]reader/meta_reader/meta_reader/main.cpp
+++ [hw3_lecture]reader/meta_reader/meta_reader/main.cpp
@@ -170,18 +170,30 @@ public:
 	Reader(const std::string& filename) : infile_(filename, std::ios::binary), position_(0) {
 		// RAII
 		total_typelist_size_ = SizeOf<TList>::value;
+		std::cout << "Total size of types in the TypeList: " << total_typelist_size_ << std::endl;
 	}
 
 	void* readNextLine() {
 
 		infile_.seekg(position_);  // set the current position
 
-		delete curr_ptr_;  // delete preceeding row
-		curr_ptr_ = operator new(total_typelist_size_);
-		void *begin = curr_ptr_;
+		if (curr_ptr_ != nullptr) {
+			delete curr_ptr_;  // delete preceeding row
+		}
+		curr_ptr_ = (char *)operator new(total_typelist_size_);
 
 		infile_.read(curr_ptr_, total_typelist_size_);
 		position_ = infile_.tellg(); // get the current position
+
+		std::cout << "Current position: " << position_ << std::endl;
+
+		/*
+		for (int i: Length<TList>::value) {
+			TypeAt<TList, i>::Result *ptr = new TypeAt<TList, i>::Result;
+
+		}
+		
+		*/
 
 		/*
 		for (int i : Length<TList>) {
@@ -197,7 +209,7 @@ public:
 		end_position += std::sizeof(curr_type);
 		*/
 
-		return begin;  // or just curr_ptr_?
+		return curr_ptr_;
 	}
 
 	~Reader() {
@@ -208,17 +220,50 @@ private:
 	std::size_t total_typelist_size_;
 	std::ifstream infile_;
 	std::size_t position_;
-	void *curr_ptr_;
+	char *curr_ptr_;
 };
 
 int main() {
-	const std::string filename = "./test.txt";
+	// open ouput stream and wrie the test data
+	const std::string filename = "./test.bin";
 
+	std::ofstream OutFile(filename, std::ios::out | std::ios::binary);
+
+	int int1 = 100;
+	char char1 = 'a';
+	int int2 = -5432;
+	double double1 = 12.345;
+
+	OutFile.write(reinterpret_cast<const char*>(&int1), sizeof(int));
+	OutFile.write(reinterpret_cast<const char*>(&char1), sizeof(char));
+	OutFile.write(reinterpret_cast<const char*>(&int2), sizeof(int));
+	OutFile.write(reinterpret_cast<const char*>(&double1), sizeof(double));
+
+	OutFile.write(reinterpret_cast<const char*>(&int1), sizeof(int));
+	OutFile.write(reinterpret_cast<const char*>(&char1), sizeof(char));
+	OutFile.write(reinterpret_cast<const char*>(&int2), sizeof(int));
+	OutFile.write(reinterpret_cast<const char*>(&double1), sizeof(double));
+
+	OutFile.write(reinterpret_cast<const char*>(&int1), sizeof(int));
+	OutFile.write(reinterpret_cast<const char*>(&char1), sizeof(char));
+	OutFile.write(reinterpret_cast<const char*>(&int2), sizeof(int));
+	OutFile.write(reinterpret_cast<const char*>(&double1), sizeof(double));
+
+	OutFile.close();
+	
+	// read from binary file line by line
+	// get types from the TypeList 
 	Reader<TypeList<int, char, int, double>> reader(filename);
 
+	void *ptr;
 	for (int i = 0; i < 3; i++) {
-		reader.readNextLine();
+		std::cout << "Reading new line.." << std::endl;
+		ptr = reader.readNextLine();
+		std::cout << "Current pointer address: " << ptr << std::endl;
 	}
+
+	int _;
+	std::cin >> _;
 
 	return 0;
 }
